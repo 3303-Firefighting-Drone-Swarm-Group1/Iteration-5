816c702ef20e2edd44d9807bc1262868f30488b0
Marc <marc63570@gmail.com>
2025-04-06 20:15:15 -0400

    fixed errors in TaskMessage

--- Iteration 5/test/TaskMessageTest.java
+++ Iteration 5/test/TaskMessageTest.java
@@ -4,10 +4,10 @@ import static org.junit.Assert.*;
 public class TaskMessageTest {
     @Test 
     public void testTaskMessage() {
-        private double water = 42.0;
-        private Point fireLocation = new Point(2.0, 5.0);
-        private Point droneLocation = new Point(3.0, 6.0);
-        private Incident.Fault fault = Incident.Fault.DRONE_STUCK;
+        double water = 42.0;
+        Point fireLocation = new Point(2.0, 5.0);
+        Point droneLocation = new Point(3.0, 6.0);
+        Incident.Fault fault = Incident.Fault.DRONE_STUCK;
 
         TaskMessage tm = new TaskMessage(water, fireLocation, droneLocation, fault);
 
