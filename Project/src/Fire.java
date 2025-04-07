37cb5562e9ed4d1ca9c8a130bebb872ef56ac0c6
lucaswarburton <lucas.warburton@gmail.com>
2025-04-07 10:15:30 -0400

    Added javadoc to Fire

--- Project/src/Fire.java
+++ Project/src/Fire.java
@@ -1,3 +1,6 @@
+/**
+ * Entity class used to represent a fire
+ */
 public class Fire {
     private double water; //in liters
     private double x, y;
@@ -23,34 +26,65 @@ public class Fire {
         }
     }
 
+    /**
+     * Gets the amount of water needed to put out the fire
+     * @return The amount of water needed in L
+     */
     public double getWater(){
         return water;
     }
 
+    /**
+     * Puts water on the fire
+     * @param amount The amount of water added
+     */
     public void putWater(double amount){
         water -= amount;
     }
 
+    /**
+     * Gets the x coordinate of the fire
+     * @return the x coordinate
+     */
     public double getX(){
         return x;
     }
 
+    /**
+     * Gets the y coordinate of the fire
+     * @return the y coordinate
+     */
     public double getY(){
         return y;
     }
 
+    /**
+     * Gets the severity of the fire
+     * @return the severity
+     */
     public Incident.Severity getSeverity(){
         return severity;
     }
 
+    /**
+     * Gets the distance of the fire from the origin
+     * @return the distance from the origin
+     */
     public double getDistance(){
         return Point.distance(x, y, 0, 0);
     }
 
+    /**
+     * Gets the fault status of the fire
+     * @return the fault
+     */
     public Incident.Fault getFault(){
         return fault;
     }
 
+    /**
+     * Clears the fault from the fire
+     */
     public void clearFault(){
         fault = Incident.Fault.NONE;
     }
