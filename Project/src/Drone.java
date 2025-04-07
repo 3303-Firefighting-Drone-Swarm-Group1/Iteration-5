e919e34f4a47082d71718ae8e3f21dfa83d03c08
lucaswarburton <lucas.warburton@gmail.com>
2025-04-07 10:04:53 -0400

    Added javadoc for Drone

--- Project/src/Drone.java
+++ Project/src/Drone.java
@@ -1,3 +1,7 @@
+/**
+ * Entity object used to store a representation of a drone.
+ */
+
 public class Drone {
     private RPCClient client;
     private DroneSubsystem.DroneState state;
@@ -14,48 +18,97 @@ public class Drone {
         vY = 0;
     }
 
+    /**
+     * Sets the state of the drone
+     * @param state The new state
+     */
     public void setState(DroneSubsystem.DroneState state){
         this.state = state;
     }
 
+    /**
+     * Sets the velocity of the drone in m/s
+     * @param x the new x velocity
+     * @param y the new y velocity
+     */
     public void setVelocity(double x, double y){
         vX = x;
         vY = y;
     }
 
+    /**
+     * Gets the x coordinate of the drone
+     * @return the x coordinate
+     */
     public double getX(){
         return x;
     }
 
+    /**
+     * Gets the y coordinate of the drone
+     * @return the y coordinate
+     */
     public double getY(){
         return y;
     }
 
+    /**
+     * Gets the x velocity of the drone
+     * @return the x velocity
+     */
     public double getVX(){
         return vX;
     }
 
+    /**
+     * Gets the y velocity of the drone
+     * @return the y velocity
+     */
     public double getVY(){
         return vY;
     }
 
+    /**
+     * Sets the location of the drone
+     * @param x the new x coordinate
+     * @param y the new y coordinate
+     */
     public void setLocation(double x, double y){
         this.x = x;
         this.y = y;
     }
 
+    /**
+     * Gets the state of the drone
+     * @return the state
+     */
     public DroneSubsystem.DroneState getState(){
         return state;
     }
 
+    /**
+     * Sends an RPC message to the DroneSubsystem associated with this Drone
+     * @param request A serializable object to be sent to the DroneSubsystem
+     * @return The return message
+     */
     public Object sendRequest(Object request){
         return client.sendRequest(request);
     }
 
+    /**
+     * Sends an RPC message to the DroneSubsystem associated with this Drone
+     * @param request A serializable object to be sent to the DroneSubsystem
+     * @param timeout The time allowed for the DroneSubsystem to respond
+     * @return The return message
+     */
     public Object sendRequest(Object request, int timeout){
         return client.sendRequest(request, timeout);
     }
 
+    /**
+     * Gets the port of the DroneSubsystem associated with the Drone
+     * @return
+     */
     public int getPort(){
         return client.getPort();
     }
